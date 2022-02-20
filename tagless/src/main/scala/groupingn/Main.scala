e11ea63ab0f063b93037e426357acf2022e2c87f
y2k2mt <y2.k2mt@gmail.com>
2022-02-20 23:49:54 +0900

    Add type alias

--- tagless/src/main/scala/groupingn/Main.scala
+++ tagless/src/main/scala/groupingn/Main.scala
@@ -5,10 +5,12 @@ import models.interpretors.implicits.*
 
 object Main extends IOApp {
 
+  type Effect[T] = IO[T]
+
   def run(args: List[String]) = {
     for {
-      _ <- Database.initialize[IO].to[IO]
-      s <- Server.stream[IO].compile.drain.as(ExitCode.Success).to[IO]
+      _ <- Database.initialize[Effect].to[Effect]
+      s <- Server.stream[Effect].compile.drain.as(ExitCode.Success).to[Effect]
     } yield s
   }
 }
